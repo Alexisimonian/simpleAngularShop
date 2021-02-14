be2f0ffa75974683bfb192e4e084d0d79924ccb6
Alexis Simonian <alexis792.as@gmail.com>
2021-02-14 17:13:30 +0100

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductModule } from './products/product/product.module';
+import { ProductModule } from './products/product.module';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ProductModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ProductModule],
+  declarations: [AppComponent, HomeComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ShareModule } from '../../shared/share/share.module';
-
-
+import { ShareModule } from '../shared/share.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    ShareModule
-  ]
+  imports: [CommonModule, ShareModule],
 })
-export class ProductModule { }
+export class ProductModule {}
